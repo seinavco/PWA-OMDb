4235ea5dc614373bc537756e731ef6448831b70f
einavc <einav.cohen@xmpie.com>
2019-05-04 14:20:01 +0300

    Connecting to imdb api

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import store from './store';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+ReactDOM.render(
+  (
+    // Provide our store to the application
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ),
+  document.getElementById('root'));
+registerServiceWorker();
